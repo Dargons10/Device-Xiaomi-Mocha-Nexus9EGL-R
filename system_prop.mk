7133348104add3e3f2eb05c41b3fa9f2c79c2a5f
Dargons10 <jdamr2p@gmail.com>
2024-04-16 14:50:04 +0200

    Put correct adb prop

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,13 @@
+#ADB
+PRODUCT_PROPERTY_OVERRIDES += \
+  persist.service.adb.enable=1 \
+  persist.service.debuggable=1 \
+  persist.sys.usb.config=mtp,adb \
+  persist.sys.root_access=2 \
+  ro.secure=0 \
+  ro.adb.secure=0 \
+  ro.debuggable = 1 \
+
 # API
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.first_api_level=23
@@ -104,17 +114,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ap.interface=wlan0 \
     persist.tegra.nvmmlite = 1 \
     persist.wlan.ti.calibrated = 0
-
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-  persist.sys.usb.config=mtp
-
-PRODUCT_PROPERTY_OVERRIDES += \
-  persist.service.adb.enable=1 \
-  persist.service.debuggable=1 \
-  persist.sys.usb.config=mtp,adb \
-  ro.secure=0 \
-  ro.adb.secure=0 
 
 # Vendor Patch Level
 PRODUCT_PROPERTY_OVERRIDES += \
