9267226aa30046456eee98756e3450d2d10afaca
apple <105837597+380583067@users.noreply.github.com>
2025-03-19 20:40:46 +0800

    Update the correct definition of USB for AOSP

--- mocha.mk
+++ mocha.mk
@@ -336,7 +336,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service
 
 # Use legacy ADB USB support
 PRODUCT_PROPERTY_OVERRIDES += \
