f1cc096521d21147d45f84496aec60028d4d46fa
apple <105837597+380583067@users.noreply.github.com>
2025-03-19 23:14:22 +0800

    Change compilation settings to WiFi tablet

--- lineage_mocha.mk
+++ lineage_mocha.mk
@@ -1,16 +1,20 @@
 # Inherit device configuration for mocha.
 $(call inherit-product, device/xiaomi/mocha/full_mocha.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+# Boot Animtion
+TARGET_BOOTANIMATION_HALF_RES := true
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := lineage_mocha
+
+TARGET_GAPPS_ARCH := arm
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := aosp_mocha
 PRODUCT_DEVICE := mocha
-PRODUCT_BAND := xiaomi
+PRODUCT_BRAND := xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 BOARD_VENDOR := Xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Xiaomi/carbon_mocha/mocha:5.1.1/LMY49J/7fd38a3d2b:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
