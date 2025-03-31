1f3bef5c1b4b0135a4c1d4da47a972caa0a94b33
dargons10 <jdamr2p@gmail.com>
2025-03-31 15:56:54 +0200

    Revert "Change compilation settings to WiFi tablet"
    This reverts commit f1cc096521d21147d45f84496aec60028d4d46fa.

--- lineage_mocha.mk
+++ lineage_mocha.mk
@@ -1,20 +1,16 @@
 # Inherit device configuration for mocha.
 $(call inherit-product, device/xiaomi/mocha/full_mocha.mk)
 
-# Boot Animtion
-TARGET_BOOTANIMATION_HALF_RES := true
 # Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
-
-TARGET_GAPPS_ARCH := arm
-TARGET_BOOT_ANIMATION_RES := 1080
-
-PRODUCT_NAME := aosp_mocha
+PRODUCT_NAME := lineage_mocha
 PRODUCT_DEVICE := mocha
-PRODUCT_BRAND := xiaomi
+PRODUCT_BAND := xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 BOARD_VENDOR := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Xiaomi/carbon_mocha/mocha:5.1.1/LMY49J/7fd38a3d2b:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
