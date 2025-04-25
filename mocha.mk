91314cece1daf8ed2fe85d1aeaea75dcc5fd1669
Dargons10 <jdamr2p@gmail.com>
2025-04-25 16:10:18 +0200

    mocha: device: Drop overlay-lineage

--- mocha.mk
+++ mocha.mk
@@ -224,8 +224,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
         
 # Permissions
 PRODUCT_COPY_FILES += \
