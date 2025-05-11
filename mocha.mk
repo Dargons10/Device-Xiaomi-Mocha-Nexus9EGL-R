e73718591c6878019587375d64dd6ff4061a1e25
Dargons10 <jdamr2p@gmail.com>
2025-05-11 22:01:05 +0200

    mocha: Drop nvpu_shims and other initfiles

--- mocha.mk
+++ mocha.mk
@@ -298,11 +298,7 @@ PRODUCT_PACKAGES += \
     init.tn8_common.rc \
     init.ussrd.rc \
     power.tn8.rc \
-    power.mocha.rc \
-    ueventd.tn8.rc \
-    ussrd.conf \
-    init.nvgpu_shims.rc \
-    ussr_setup \
+    ueventd.tn8.rc 
   
 
 PRODUCT_COPY_FILES += \
