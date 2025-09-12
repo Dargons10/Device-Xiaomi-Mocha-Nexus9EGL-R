b750e3a0a468cd5d8fb9c40e54db4269a20b40fe
Dargons10 <jdamr2p@gmail.com>
2025-09-12 09:30:03 +0200

    mocha: audio: Update to audio@6.0 HIDL

--- mocha.mk
+++ mocha.mk
@@ -31,10 +31,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0 \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@6.0 \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
     audio.a2dp.default \
     audio.usb.default \
     audio.r_submix.default \
