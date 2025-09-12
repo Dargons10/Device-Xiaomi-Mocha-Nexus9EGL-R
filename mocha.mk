d31f7871308056e8b6deb649d625ec094d5eaadf
Dargons10 <jdamr2p@gmail.com>
2025-09-12 13:17:51 +0200

    mocha: thermal: back to old version

--- mocha.mk
+++ mocha.mk
@@ -313,8 +313,9 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-service-nvidia \
-    thermalhal.tn8.xml
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.tn8.xml
 
 # TimeKeep
 PRODUCT_PACKAGES += \
--- thermal/Android.mk
+++ thermal/Android.mk
@@ -15,7 +15,7 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE        := thermalhal.tn8.xml
+LOCAL_MODULE        := thermal.tn8.xml
 LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES     := thermalhal.tn8.xml
