a6673a601a09c8a4f028ada04c7ea2114cf3cb65
Dargons10 <jdamr2p@gmail.com>
2025-09-15 13:40:28 +0200

    mocha: shims: Fix typo

--- shims/Android.mk
+++ shims/Android.mk
@@ -23,7 +23,7 @@ LOCAL_MULTILIB         := 32
 LOCAL_SHARED_LIBRARIES := libstagefright libui
 LOCAL_VENDOR_MODULE    := true
 include $(BUILD_SHARED_LIBRARY)
-
+
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := stdio_vsnprintf.cpp
 LOCAL_C_INCLUDES := \
