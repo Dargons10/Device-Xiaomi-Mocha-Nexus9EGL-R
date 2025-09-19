92089a0207644deffe581d6a13e688329e33c7fa
Dargons10 <jdamr2p@gmail.com>
2025-09-19 14:40:23 +0200

    mocha: system_prop: Fix APTX

--- system_prop.mk
+++ system_prop.mk
@@ -17,7 +17,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.bt.enableAptXHD=true \
     persist.service.btui.use_aptx=1 \
     persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
-    persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldacs
+    persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac
 
 # BT
 PRODUCT_PROPERTY_OVERRIDES += \
